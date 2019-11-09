972791f0ed46ff0b64788ec34b7035cdd9943889
Mariyan Apostolov <mariyan_apostolov@mail.bg>
2019-11-09 23:01:42 +0200

    ORM in Node fix

--- Muzeiko/NodeJs_Junior_Developers/02_ORM_in_Node/config/db.js
+++ Muzeiko/NodeJs_Junior_Developers/02_ORM_in_Node/config/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const settings = require('../settings');
 const kittyEnttity = require('../entities/kittyEntity');
 
-const sequelize = new Sequelize('shop_03', 'test_user', '12345678', settings.mysqlLogin);
+const sequelize = new Sequelize(settings.logIn.database, settings.logIn.username, settings.logIn.password, settings.mysqlLogin);
 
 module.exports = {
     dbConfig: (app) => {
--- Muzeiko/NodeJs_Junior_Developers/02_ORM_in_Node/settings.js
+++ Muzeiko/NodeJs_Junior_Developers/02_ORM_in_Node/settings.js
@@ -1,5 +1,10 @@
 module.exports = {
     port: 8081,
+    logIn: {
+        database: 'shop_03',
+        username: 'test_user',
+        password: '12345678'
+    },
     mysqlLogin: {
         host: 'localhost',
         dialect: 'mysql'
