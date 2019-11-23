b941c09f322a5c6500fe191712e7487fec9955f9
Mariyan Apostolov <mariyan_apostolov@mail.bg>
2019-11-24 00:52:34 +0200

    NodeJs Jun Devs - Create new db fix

--- Muzeiko/NodeJs_Junior_Developers/02_ORM_in_Node/config/db.js
+++ Muzeiko/NodeJs_Junior_Developers/02_ORM_in_Node/config/db.js
@@ -9,7 +9,7 @@ module.exports = {
         return new Promise(function(resolved, rejected) {
             kittyEnttity
                 .kittyConfig(sequelize, Sequelize)
-                .sync({ force: true })
+                .sync({ alter: false })
                 .then(() => {
                     resolved(app);
                 })
